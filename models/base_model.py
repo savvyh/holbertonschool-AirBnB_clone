4e55dbba238e241b485bfdf87a8f27b9a722ad97
savvyh <7277@holbertonstudents.com>
2024-03-05 13:41:28 +0000

    fix github issue

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,6 @@ class BaseModel():
     def to_dict(self):
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat(),
-        new_dict['updated_at'] = self.updated_at.isoformat(),
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
