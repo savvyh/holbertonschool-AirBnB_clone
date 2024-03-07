e35cd8efdc994b80831a6a2f84d566e8a4cccea5
savvyh <7277@holbertonstudents.com>
2024-03-07 10:14:25 +0000

    Fix pycodestyle

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Subclass(User), represent an users with them informations.
-    
+
     Public class attributes:
         email, password, first_name, last_name
     """
